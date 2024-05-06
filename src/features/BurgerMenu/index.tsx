70b4e70a8738dbbfc24a9984b85c5f8b1233dbc4
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-05-06 19:58:04 +0600

    --ADDED BURGERMENU

--- src/features/BurgerMenu/index.tsx
+++ src/features/BurgerMenu/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { navLinks } from "@shared/lib/links";
+
+import styles from "./styles.module.scss";
 
 export const BurgerMenu = () => {
-  return <div></div>;
+  return (
+    <div className={styles.burger_menu}>
+      {navLinks.map((link) => (
+        <a href={link.href} className={styles.burger_menu__link}>
+          {link.label}
+        </a>
+      ))}
+    </div>
+  );
 };
