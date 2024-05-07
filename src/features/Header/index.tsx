3362a34333c9c1706ef61ac1a37fb8193b8a697f
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-05-07 13:18:22 +0600

    --BURGER ANIMATION IS WORKING

--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -1,14 +1,16 @@
 import { BurgerMenu } from "@features/BurgerMenu";
 import { useState } from "react";
 import { useMenuAnimation } from "@shared/lib/hooks/useMenuAnimation";
+import { useBurgerButtonAnimation } from "@shared/lib/hooks/useBurgerButtonAnimation";
 import { Logo } from "@entities/Logo";
 import { BurgerButton } from "@shared/ui/BurgerButton";
 
 import styles from "./styles.module.scss";
 
 export const Header = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const { menuRef } = useMenuAnimation(isOpen);
+  const { firstLine, secondLine, thirdLine } = useBurgerButtonAnimation(isOpen);
 
   const handleOpenMenu = () => {
     setIsOpen(!isOpen);
@@ -18,7 +20,13 @@ export const Header = () => {
     <header className={styles.header_mob}>
       <Logo />
       <BurgerMenu menuRef={menuRef} isOpen={isOpen} />
-      <BurgerButton isOpen={isOpen} onClick={handleOpenMenu} />
+      <BurgerButton
+        isOpen={isOpen}
+        onClick={handleOpenMenu}
+        firstLine={firstLine}
+        secondLine={secondLine}
+        thirdLine={thirdLine}
+      />
     </header>
   );
 };
